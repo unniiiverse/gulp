7806c4cc1fc274ec77644560ae957d68c952c594
uni <unniiiverse@gmail.com>
2022-02-27 22:31:22 +0200

    add: webpack, support ES6 modules

--- gulpfile.js
+++ gulpfile.js
@@ -15,18 +15,20 @@ global.app = {
 // Импорт задач
 import { copy } from "./gulp/tasks/copy.js"; // Сборка всех файлов из src/ в dist/ 
 import { cleaner } from "./gulp/tasks/cleaner.js"; // Очистка папки перед следующим запуском сборщика
-import { html } from "./gulp/tasks/html.js"; // Всё связанное с HTML
+import { html } from "./gulp/tasks/html.js"; // Обработка HTML файлов
 import { server } from "./gulp/tasks/server.js"; // Локальный сервер
 import { scss } from "./gulp/tasks/scss.js"; // Обработка SASS(SCSS) файлов
+import { js } from "./gulp/tasks/js.js"; // Обработка Java Script файлов
 
 // Наблюдатель за изменениями
 function watcher() {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss)
+const mainTasks = gulp.parallel(copy, html, scss, js);
 
 // Постоение сценаривев
 const dev = gulp.series(cleaner, mainTasks, gulp.parallel(watcher, server));
